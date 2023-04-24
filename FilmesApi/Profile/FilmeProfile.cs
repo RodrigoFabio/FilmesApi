61f6b0fda9f736850212b5c505d9f453f53d4d67
Rodrigo Fabio Lima <rodrigo.amorim452@gmail.com>
2023-04-24 10:03:30 -0400

    Adicionando novos arquivos

---
+++ FilmesApi/Profile/FilmeProfile.cs
@@ -0,0 +1,17 @@
+﻿using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+using AutoMapper;
+
+namespace FilmesApi.Profiles
+{
+    public class FilmeProfile: Profile
+    {
+        public FilmeProfile()
+        {
+            CreateMap<CreateFilmeDto, Filme>();
+            CreateMap<UpdateFilmeDto, Filme>();
+            CreateMap<Filme, UpdateFilmeDto>();
+            CreateMap<Filme, ReadFilmeDto>();
+        }
+    }
+}
